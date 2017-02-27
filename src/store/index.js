542c5bcf72936fc9e332188ca4ab336b1a6a228f
xeeshan <zeeshanansari38244@gmail.com>
2017-02-26 22:52:54 -0800

    store with subscribe

--- src/store/index.js
+++ src/store/index.js
@@ -4,6 +4,9 @@ import root_reducer from './reducers/root_reducer'
 
 let store = createStore(root_reducer);
 
-
+// getState check  for current state 
+store.subscribe(() =>
+  console.log(store.getState())
+);
 
 export default store;
